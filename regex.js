55d761368a6fc87a182d867c662218c76f366cc1
RKeenan <RKeenan@onestaffmedical.com>
2021-05-20 09:57:05 -0500

    updated with RFC 2822 email regex

--- regex.js
+++ regex.js
@@ -1,8 +1,14 @@
 function regexTest() {
-  let regex = /^((?!\.)[\w-_.]*[^.])(@\w+)(\.\w+(\.\w+)?[^.\W])$/gm;
+  let regex =
+    /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
   let input = document.getElementById("inputField").value;
-  let output = input.replace(regex, input);
-  console.log(output);
+  //let output = input.replace(regex, input);
+  if (input.match(regex)) {
+    let output = input.replace(regex, input);
+    console.log(output);
+  } else {
+    console.log("This does not match the regex.");
+  }
 }
 
 // let email = "rkeenan@gmail.com";
@@ -13,4 +19,3 @@ function regexTest() {
 
 // console.log(email);
 // console.log(email2);
-
