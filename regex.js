b6956b77ce06f5bc9f880285eecd651ccfdaa0f6
Ross Keenan <ross.keenan@protonmail.com>
2021-05-19 19:55:54 -0500

    Completed Demo for work

---
+++ regex.js
@@ -0,0 +1,15 @@
+function regexTest() {
+  let regex = /^((?!\.)[\w-_.]*[^.])(@\w+)(\.\w+(\.\w+)?[^.\W])$/gm;
+  let input = document.getElementById("inputField").value;
+  let output = input.replace(regex, input);
+  console.log(output);
+}
+
+// let email = "rkeenan@gmail.com";
+// let email2 = "rkeenan@gmail.com.com";
+
+// let reEmail = email.replace(regex, email);
+// let reEmail2 = email.replace(regex, email2);
+
+// console.log(email);
+// console.log(email2);
